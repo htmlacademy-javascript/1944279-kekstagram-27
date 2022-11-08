4f795873985238ad4657450f3d2b4b60dfcfb812
Iulia <bezrejenka@gmail.com>
2022-11-08 18:26:08 +0300

    Доделывает удаление обработчика ESC и добавляет валидацию

---
+++ js/big-picture.js
@@ -0,0 +1,54 @@
+import {isEscapeKey} from './util.js';
+
+const bigPicture = document.querySelector('.big-picture');
+const commentsList = bigPicture.querySelector('.social__comments');
+const commentTemplate = commentsList.querySelector('.social__comment');
+const commentsFragment = document.createDocumentFragment();
+const closePictureButton = bigPicture.querySelector('.big-picture__cancel');
+
+const drawBigPicture = ({url, likes, comments, description}) => {
+  bigPicture.classList.remove('hidden');
+  document.body.classList.add('.modal-open');
+
+  bigPicture.querySelector('.big-picture__img img').src = url;
+  bigPicture.querySelector('.likes-count').textContent = likes;
+  bigPicture.querySelector('.comments-count').textContent = comments.length;
+  bigPicture.querySelector('.social__caption').textContent = description;
+
+  for (const comment of comments){
+    const newComment = commentTemplate.cloneNode(true);
+    const {avatar, name, message} = comment;
+
+    newComment.querySelector('img').src = avatar;
+    newComment.querySelector('img').alt = name;
+    newComment.querySelector('.social__text').textContent = message;
+
+    commentsFragment.append(newComment);
+  }
+  commentsList.innerHTML = '';
+  commentsList.append(commentsFragment);
+
+  bigPicture.querySelector('.social__comment-count').classList.add('hidden');
+  bigPicture.querySelector('.comments-loader').classList.add('hidden');
+
+  const hidePictureModal = () =>{
+    document.body.classList.remove('modal-open');
+    bigPicture.classList.add('hidden');
+  };
+
+  closePictureButton.addEventListener('click', () => {
+    hidePictureModal();
+  });
+
+  const onModalEscKeydown = (evt) => {
+    if (isEscapeKey(evt)) {
+      hidePictureModal();
+    }
+  };
+
+  document.addEventListener('keydown', onModalEscKeydown);
+
+};
+
+
+export {drawBigPicture};
--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import {createPhotosArray} from './data.js';
 import {drawPictures} from './picture.js';
-
+import {drawBigPicture} from './big-picture.js';
+import './form.js';
 
 drawPictures(createPhotosArray());
+
 
 
