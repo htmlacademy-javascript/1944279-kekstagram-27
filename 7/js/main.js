bf9cfaf414626b5cdee5cfbd0a2d33aa0cc06c08
Keks <keks@htmlacademy.ru>
2022-11-08 21:20:34 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/main.js
@@ -0,0 +1,7 @@
+import {createPhotosArray} from './data.js';
+import {drawPictures} from './picture.js';
+import {drawBigPicture} from './big-picture.js';
+import './form.js';
+
+drawPictures(createPhotosArray());
+
