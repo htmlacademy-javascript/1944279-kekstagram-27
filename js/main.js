14f847cbae694a8473c00d406140d9257575fd90
Iulia <bezrejenka@gmail.com>
2022-11-09 12:01:20 +0300

    Исправляет ошибки

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 import {createPhotosArray} from './data.js';
 import {drawPictures} from './picture.js';
-import {drawBigPicture} from './big-picture.js';
+import './big-picture.js';
 import './form.js';
 
 drawPictures(createPhotosArray());
+
 
