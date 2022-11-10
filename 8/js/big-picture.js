3c80dd4b786225fc55969407705a95dab49b492b
Keks <keks@htmlacademy.ru>
2022-11-10 16:30:10 +0300

    :heavy_check_mark: Сборка #8

--- 8/js/big-picture.js
+++ 8/js/big-picture.js
@@ -1,5 +1,7 @@
 import {isEscapeKey} from './util.js';
 
+const NUMBER_OF_COMMENTS_TO_LOAD = 5;
+
 const bigPicture = document.querySelector('.big-picture');
 const commentsList = bigPicture.querySelector('.social__comments');
 const commentTemplate = commentsList.querySelector('.social__comment');
@@ -7,6 +9,7 @@ const commentsFragment = document.createDocumentFragment();
 const closePictureButton = bigPicture.querySelector('.big-picture__cancel');
 const commentCount = document.querySelector('.social__comment-count');
 const commentLoaderButton = document.querySelector('.comments-loader');
+
 
 const drawBigPicture = ({url, likes, comments, description}) => {
   bigPicture.classList.remove('hidden');
@@ -20,7 +23,7 @@ const drawBigPicture = ({url, likes, comments, description}) => {
   commentsList.innerHTML = '';
   let numberOfComments = 0;
   const commentLoad = (commentsArray, index) => {
-    for (let i = index; i < index + 5 && i < commentsArray.length; i++) {
+    for (let i = index; i < index + NUMBER_OF_COMMENTS_TO_LOAD && i < commentsArray.length; i++) {
       const newComment = commentTemplate.cloneNode(true);
       const {avatar, name, message} = commentsArray[i];
 
