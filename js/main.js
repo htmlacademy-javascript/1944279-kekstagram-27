08fb4ff8c5d04071c36bbe2a7e775e7adbf0290a
Iulia <bezrejenka@gmail.com>
2022-11-18 14:36:48 +0300

    Доделывает форму загрузки изображения

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-import './form.js';
 import './gallery.js';
+import './form.js';
+import './picture-preview.js';
 import './scale.js';
 import './effects.js';
-import './picture-preview.js';
+
--- js/picture-preview.js
+++ js/picture-preview.js
@@ -3,7 +3,6 @@ const FILE_TYPES = ['jpg', 'jpeg', 'png'];
 const fileChooser = document.querySelector('.img-upload__input');
 const preview = document.querySelector('.img-upload__preview img');
 
-
 fileChooser.addEventListener('change', () => {
   const file = fileChooser.files[0];
   const fileName = file.name.toLowerCase();
